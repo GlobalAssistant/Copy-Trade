8715d5c1c85220ec295d3cdedd7fa1517d772580
Top Coder <leeyamkeng120@gmail.com>
2022-01-17 14:47:21 +0000

    first

---
+++ app/engine.py
@@ -0,0 +1,293 @@
+# from entity.OpenPosition import OpenPosition
+import requests
+import time
+from datetime import datetime, timezone
+from binance_f import RequestClient
+from binance_f import SubscriptionClient
+from binance_f.constant.test import *
+from binance_f.model import *
+from binance_f.exception.binanceapiexception import BinanceApiException
+from binance_f.base.printobject import *
+import json
+
+from models import *
+from start import app, db
+  
+# Opening JSON file
+with open('config.json', 'r') as openfile:
+	json_object = json.load(openfile)
+
+keys = json_object["master"]
+zignaly_keys = json_object["slave"]
+binance_futures_leverage = json_object["leverage"]
+
+def resolvePositionKey(order):
+	return order
+
+def sendPost(request, zignaly_keys):
+
+	# r = requests.post(zignaly_keys["url"], 
+	#			 data={
+	#				 "pair": request.getPair(),
+	#				 "signalId": request.getSignalId(),
+	#				 "type": request.getOtype(),
+	#				 "exchange": request.getExchange(),
+	#				 "exchangeAccountType": request.getExchangeAccountType(),
+	#				 "side": getSide(),
+	#				 "orderType":  request.getOrderType(),
+	#				 "leverage": "10",
+	#				 "positionSizePercentage": getPositionSizePercentage(),
+	#				 "key": request.getKey(),
+	#				 "stopLossFollowsTakeProfit":True
+	#			 }
+	#	 )
+
+	r = requests.post(zignaly_keys["url"], 
+				data={
+					"pair": request.getPair(),
+					"signalId": "long1id",
+					"type": "entry",
+					"exchange": "zignaly",
+					"exchangeAccountType": "futures",
+					"side": "long",
+					"orderType": "market",
+					"leverage": "10",
+					"positionSizePercentage": "1",
+					"key": zignaly_keys["api_key"],
+					"stopLossFollowsTakeProfit":True
+				}
+		)
+
+def savePosition(order, position):
+	position.updateDate = datetime.now(timezone.utc)
+	position_json = json.dumps(position.__dict__)
+	position_str = str(position_json)
+	q = positionString(positionKey=resolvePositionKey(order), positionString=position_str)
+	db.session.add(q)
+	db.session.commit()
+
+def createSignal(position, side, otype, positionSizePercentage):
+	request = CreateSignalRequest()
+	request.setLeverage(binance_futures_leverage)
+	request.setSignalId(position.getSignalId())
+	request.setPair(position.getPair())
+	reduce1 = "World" in otype
+	request.setOtype("update" if reduce1 else otype)
+	request.setSide(side);
+	request.setOrderType("MARKET")
+
+	if reduce1:
+		request.setReduceOrderType("MARKET");
+		request.setReduceAvailablePercentage(positionSizePercentage)
+	elif positionSizePercentage != None:
+		request.setPositionSizePercentage(positionSizePercentage)
+
+	# zignalyService.createSignal(request)
+	request.setKey(zignaly_keys["api_key"])
+	request.setExchange("binance")
+	request.setExchangeAccountType("futures")
+
+	db.session.add(request)
+	db.session.commit()
+
+	sendPost(request, zignaly_keys)
+
+def resolvePositionKey(order):
+	return order.positionSide + "-" + order.symbol;
+
+def processOrderUpdate(order):
+	if order.orderStatus != "FILLED":
+		print("Order is not FILLED")
+		return False
+
+	positionKey = resolvePositionKey(order)
+	# check eventTime to prevent double processing
+	orderKey = "ORDER-" + positionKey
+	lastOrderEvent = LastOrderEvent.query.filter_by(orderKey=orderKey)
+
+	# todo UPD: check if no concurrent issues for orders processing
+	if int(lastOrderEvent) >= int(orderTradeTime):
+		print("This order already processed for position", positionKey)
+		return False
+	else:
+		# Instead of Redis
+		q = LastOrderEvent(orderKey=orderKey, lastorderevent=string(lastOrderEvent))
+		db.session.add(q)
+		db.session.commit()
+
+
+	# Check whether it is a new postion or exsited position
+	positionString = positionString.query.filter_by(positionKey=positionKey)
+	positionSizePercentage = None
+	positionSize = round(order.origQty * order.avgPrice / binance_futures_leverage, 2)
+
+	if positionString == None:
+		side = "LONG" if order.side == "BUY" else "SELL"
+		# q = OpenPosition(signalId=unique_random_id, pair=order.symbol, side=side, quantity=order.origQty, maxQuantity=order.origQty, positionSize=positionSize, createDate=createDate, updateDate=datetime.now(timezone.utc), lastEventTime=datetime.now(timezone.utc))
+		position = OpenPosition()
+		position.signalId=unique_random_id
+		position.pair=order.symbol
+		position.quantity=order.origQty
+		position.maxQuantity=order.origQty
+		position.positionSize=positionSize
+		position.createDate=createDate
+		position.updateDate=datetime.now(timezone.utc)
+		position.lastEventTime=datetime.now(timezone.utc)
+
+		otype = "entry"
+		positionSizePercentage = round(positionSize * 100 / walletBalance, 2)
+	else:
+		position = OpenPosition()
+		position_json = json.loads(positionString)
+		if position_json["isCorrupted"] == True:
+			print("Open position is corrupted, order unable to open new position")
+			# todo: check zignaly order if still open
+			return true
+
+		if position_json["side"] != order.side:
+			print("Stored position side does not match to order side")
+			return True
+
+		increase = order.side == "BUY" if position_json["side"] == "LONG" else order.side == "SELL"
+
+		if increase :
+			# todo: if position was not reduced yet - recreate take profit
+			position.position_json["quantity"] = position_json["quantity"] + order.origQty
+			position.quantity = position.position_json["quantity"]
+			if position.position_json["quantity"] > position_json["maxQuantity"]:
+				position_json["maxQuantity"] = position_json["maxQuantity"] + order.origQty
+				position.maxQuantity = position_json["maxQuantity"]
+
+			otype = "update"
+			positionSizePercentage = round(positionSize * 100 / walletBalance, 2)
+			position_json["positionSize"] = position_json["positionSize"] + positionSize
+			position.positionSize = position_json["positionSize"]
+		else:
+			if position_json["quantity"] == position_json["maxQuantity"]:
+				# todo: if first reduce order - create stop loss order
+				print("Quantity equals with maxQuantity.")
+			result = position_json["quantity"] - order.origQty
+			if double(result) < 0:
+				print(position_json["side"] + " position " + position_json["pair"] + " quantity is negative.")
+				position_json["isCorrupted"] = True
+				position.isCorrupted = position_json["isCorrupted"]
+
+				position.signalId=position_json["signalId"]
+				position.pair=position_json["pair"]
+				position.quantity=position_json["quantity"]
+				position.maxQuantity=position_json["maxQuantity"]
+				position.positionSize=position_json["positionSize"]
+				position.createDate=position_json["createDate"]
+				position.updateDate=position_json["updateDate"]
+				position.lastEventTime=position_json["lastEventTime"]
+
+				savePosition(order, position)
+				return True
+			else:
+				otype = "update_reduce"
+				positionSizePercentage = round(positionSize * 100 / walletBalance, 2)
+				position_json["positionSize"] = position_json["positionSize"] - positionSize
+				position.positionSize = position_json["positionSize"]
+
+			walletBalance = walletBalance = order.realizedProfit()
+			position_json["quantity"] = result
+			position.quantity = position_json["quantity"]
+
+	createSignal(position, position.getSide(), order.type, positionSizePercentage)
+	# update position with order
+	if position.isClosed == True:
+		print("Closing postion " + position.pair)
+		db.session.add(position)
+		db.session.commit()
+
+		# Instead of Redis
+		positionString.query.filter_by(positionKey=positionKey).delete()
+	else:
+		savePosition(order, position)
+	return True
+
+
+def callback(data_type: 'SubscribeMessageType', event: 'any'):
+	if data_type == SubscribeMessageType.RESPONSE:
+		print("Event ID: ", event)
+	elif  data_type == SubscribeMessageType.PAYLOAD:
+		if(event.eventType == "ORDER_TRADE_UPDATE"):
+			### store filled order to db
+			print("Event Type: ", event.eventType)
+			print("Event time: ", event.eventTime)
+			print("Transaction Time: ", event.transactionTime)
+			print("Symbol: ", event.symbol)
+			print("Client Order Id: ", event.clientOrderId)
+			print("Side: ", event.side)
+			print("Order Type: ", event.type)
+			print("Time in Force: ", event.timeInForce)
+			print("Original Quantity: ", event.origQty)
+			print("Position Side: ", event.positionSide)
+			print("Price: ", event.price)
+			print("Average Price: ", event.avgPrice)
+			print("Stop Price: ", event.stopPrice)
+			print("Execution Type: ", event.executionType)
+			print("Order Status: ", event.orderStatus)
+			print("Order Id: ", event.orderId)
+			print("Order Last Filled Quantity: ", event.lastFilledQty)
+			print("Order Filled Accumulated Quantity: ", event.cumulativeFilledQty)
+			print("Last Filled Price: ", event.lastFilledPrice)
+			print("Commission Asset: ", event.commissionAsset)
+			print("Commissions: ", event.commissionAmount)
+			print("Order Trade Time: ", event.orderTradeTime)
+			print("Trade Id: ", event.tradeID)
+			print("Bids Notional: ", event.bidsNotional)
+			print("Ask Notional: ", event.asksNotional)
+			print("Is this trade the maker side?: ", event.isMarkerSide)
+			print("Is this reduce only: ", event.isReduceOnly)
+			print("stop price working type: ", event.workingType)
+			print("Is this Close-All: ", event.isClosePosition)
+			if not event.activationPrice is None:
+				print("Activation Price for Trailing Stop: ", event.activationPrice)
+			if not event.callbackRate is None:
+				print("Callback Rate for Trailing Stop: ", event.callbackRate)
+
+			order = OrderUpdate
+			order = event
+			if order != None:
+				if processOrderUpdate(order):
+					# store filled order to db
+					q = SignalOrder(orderId=event.orderId, symbol=event.symbol, side=event.side, positionSide=event.positionSide, origQty=event.origQty, avgPrice=event.avgPrice, orderTradeTime=event.orderTradeTime)
+					db.session.add(q)
+					db.session.commit()
+					flash("Order added")
+
+		elif(event.eventType == "listenKeyExpired"):
+			print("Event: ", event.eventType)
+			print("Event time: ", event.eventTime)
+			print("CAUTION: YOUR LISTEN-KEY HAS BEEN EXPIRED!!!")
+			print("CAUTION: YOUR LISTEN-KEY HAS BEEN EXPIRED!!!")
+			print("CAUTION: YOUR LISTEN-KEY HAS BEEN EXPIRED!!!")
+	else:
+		print("Unknown Data:")
+	print()
+
+def error(e: 'BinanceApiException'):
+	print(e.error_code + e.error_message)
+
+def run():
+
+	# Start user data stream
+	request_client = RequestClient(api_key=keys["api_key"], secret_key=keys["secret_key"])
+	listen_key = request_client.start_user_data_stream()
+
+	accountInformation = request_client.get_account_information_v2()
+	assets = accountInformation.assets
+	for asset in assets:
+		if asset.asset == "USDT":
+			global walletBalance
+			walletBalance = asset.walletBalance
+			print("==walletBalance: ", asset.walletBalance)
+			if walletBalance == 0:
+				print("Wallet balance is 0")
+
+	# Keep user data stream
+	result = request_client.keep_user_data_stream()
+	# print("Result: ", result)
+	client = SubscriptionClient(api_key=keys["api_key"], secret_key=keys["secret_key"])
+	client.subscribe_user_data_event(listen_key, callback, error)
---
+++ app/models.py
@@ -0,0 +1,102 @@
+from start import db
+
+class SignalOrder(db.Model):
+	__tablename__ = 'signal_order'
+	id = db.Column(db.Integer, primary_key = True)
+	orderId = db.Column(db.String(255))
+	symbol = db.Column(db.String(255))
+	side = db.Column(db.String(255))
+	positionSide = db.Column(db.String(255))
+	origQty = db.Column(db.Integer)
+	avgPrice = db.Column(db.Integer)
+	orderTradeTime = db.Column(db.String(255))
+
+	# def __init__(self, orderId, symbol, side, positionSide, origQty, avgPrice, orderTradeTime):
+	#	 self.orderId = orderId
+	#	 self.symbol = symbol
+	#	 self.side = side
+	#	 self.positionSide = positionSide
+	#	 self.origQty = origQty
+	#	 self.avgPrice = avgPrice
+	#	 self.orderTradeTime = orderTradeTime
+
+class OpenPosition(db.Model):
+	__tablename__ = 'position'
+	id = db.Column(db.Integer, primary_key = True)
+	signalId = db.Column(db.String(255))
+	pair = db.Column(db.String(255))
+	side = db.Column(db.String(255))
+	quantity = db.Column(db.Integer)
+	maxQuantity = db.Column(db.Integer)
+	positionSize = db.Column(db.Integer)
+	createDate = db.Column(db.DateTime)
+	updateDate = db.Column(db.DateTime)
+	lastEventTime = db.Column(db.String(255))
+	isClosed = db.Column(db.Boolean, default=False)
+	isCorrupted = db.Column(db.Boolean, default=False)
+
+	# def __init__(self, orderId, symbol, side, positionSide, origQty, avgPrice, orderTradeTime):
+	#	 self.orderId = orderId
+	#	 self.symbol = symbol
+	#	 self.side = side
+	#	 self.positionSide = positionSide
+	#	 self.origQty = origQty
+	#	 self.avgPrice = avgPrice
+	#	 self.orderTradeTime = orderTradeTime
+
+class LastOrderEvent(db.Model):
+	__tablename__ = 'last_order_event'
+	id = db.Column(db.Integer, primary_key = True)
+	orderKey = db.Column(db.String(255))
+	lastorderevent = db.Column(db.String(255))
+
+	# def __init__(self, orderKey, lastorderevent):
+	#	 self.orderKey = orderKey
+	#	 self.lastorderevent = lastorderevent
+
+class positionString(db.Model):
+	__tablename__ = 'positionString'
+	id = db.Column(db.Integer, primary_key = True)
+	positionKey = db.Column(db.String(255))
+	positionString = db.Column(db.Text)
+
+	# def __init__(self, positionKey, positionString):
+	#	 self.positionKey = positionKey
+	#	 self.positionString = positionString
+
+class CreateSignalRequest:
+	__tablename__ = 'CreateSignalRequest'
+	id = db.Column(db.Integer, primary_key = True)
+	key = db.Column(db.String(255))
+	exchange = db.Column(db.String(255))
+	exchangeAccountType = db.Column(db.String(255))
+	leverage = db.Column(db.Integer)
+	signalId = db.Column(db.String(255))
+	pair = db.Column(db.String(255))
+	otype = db.Column(db.String(255))
+	side = db.Column(db.String(255))
+	orderType = db.Column(db.String(255))
+	positionSizePercentage = db.Column(db.Integer)
+	reduceOrderType = db.Column(db.String(255))
+	reduceAvailablePercentage = db.Column(db.Integer)
+	limitPrice = db.Column(db.String(255))
+
+
+	# def __init__(self):
+	#	 # default/properties values
+	#	 self.key = ''
+	#	 self.exchange = ''
+	#	 self.exchangeAccountType = ''
+	#	 self.leverage = 0
+	#	 # order values
+	#	 self.signalId = ''
+	#	 self.pair = ''
+	#	 self.type = ''
+	#	 self.side = ''
+	#	 self.orderType = ''
+	#	 self.positionSizePercentage = 0
+
+	#	 self.reduceOrderType = ''
+	#	 self.reduceAvailablePercentage = 0
+	#	 self.limitPrice = ''
+		
