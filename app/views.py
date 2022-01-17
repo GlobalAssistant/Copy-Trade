62feda062e5655ec49ebc7052eaef7f67913964c
Top Coder <leeyamkeng120@gmail.com>
2022-01-17 15:02:13 +0000

    P

--- app/views.py
+++ app/views.py
@@ -95,46 +95,8 @@ def slave_form():
 
 @app.route('/')
 def homepage():
-    global test_false
 
-    if test_false == True:
-        test_false = False
-
-    final = bool(test_false) ^ bool(stop_run)
-
-    con = sql.connect("database.db")
-    con.row_factory = sql.Row
-
-    cur = con.cursor()
-    cur.execute("select * from keys where type='slave'")
-    rows = cur.fetchall()
-
-    cur = con.cursor()
-    cur.execute("select * from keys where type='master'")
-    rows2 = cur.fetchall()
-
-    slave_keys = []
-    slave_sec = []
-    master_key = []
-    master_sec = []
-
-    for row in rows:
-        slave_keys.append(row["key"])
-        slave_sec.append(row["secret"])
-
-    for row in rows2:
-        master_key.append(row["key"])
-        master_sec.append(row["secret"])
-
-    with open('config_files/config.csv', mode='w', newline='') as file:
-        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['Master API Key'] + master_key + [""])
-        writer.writerow(['Master API Keys'] + master_sec + [""])
-        writer.writerow(['Slave API Keys'] + slave_keys + [""])
-        writer.writerow(['Slave API Secrets'] + slave_sec + [""])
-
-    final_str = "No" if False else "Yes"
-    return render_template("home.html", isRunning="Is App Running ? : " + final_str, rows=rows, rows2=rows2)
+    return render_template("home.html")
 
 
 # if __name__ == "__main__":
